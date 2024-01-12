c39f8bc9b5c080c8a75f6d49a777058f98c857a4
hyunhii <be_hyunny@naver.com>
2024-01-12 19:13:34 +0900

    feat: [기본편] 1. 클래스와 데이터 - 클래스가 필요한 이유

---
+++ src/basic/class1/ClassStart1.java
@@ -0,0 +1,17 @@
+package basic.class1;
+
+public class ClassStart1 {
+
+    public static void main(String[] args) {
+        String studentName = "학생1";
+        int studentAge = 15;
+        int studentGrade = 90;
+
+        String studentName2 = "학생2";
+        int student2Age = 16;
+        int student2Grade = 80;
+
+        System.out.println("이름: " + studentName + ", 나이 :" + studentAge + ", 성적 :" + studentGrade);
+        System.out.println("이름: " + studentName2 + ", 나이 :" + student2Age + ", 성적 :" + student2Grade);
+    }
+}
