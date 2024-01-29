6906df38ce79bf9cc0ae2902d298bdfa403fdcf8
hyunhii <be_hyunny@naver.com>
2024-01-29 18:18:03 +0900

    feat: [기본편] 7. static 메서드3

--- src/basic/static2/DecoDataMain.java
+++ src/basic/static2/DecoDataMain.java
@@ -1,10 +1,12 @@
 package basic.static2;
 
+import static basic.static2.DecoData.staticCall;
+
 public class DecoDataMain {
 
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
@@ -14,6 +16,14 @@ public class DecoDataMain {
         DecoData data2 = new DecoData();
         data2.instanceCall();
 
-        DecoData.staticCall(data1);
+        staticCall(data1);
+
+        // 추가
+        // 인스턴스를 통한 접근
+        DecoData data3 = new DecoData();
+        data3.staticCall();
+
+        // 클래스를 통한 접근
+        staticCall();
     }
 }
